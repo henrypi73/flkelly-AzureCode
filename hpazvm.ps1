53b8ea1bb533eb007f71f21ecd4c8e272336bb41
Henry Pieterse (Manpower SA (Pty) Ltd) <v-hepiet@microsoft.com>
2019-08-05 10:35:32 +0200

    add tenant details

--- hpazvm.ps1
+++ hpazvm.ps1
@@ -1,3 +1,29 @@
+Function Connect-to-ARM
+{
+    Login-AzAccount
+  
+    $subscriptions = Get-AzSubscription | Sort-Object SubscriptionName | Select-Object Name,SubscriptionId
+    [int]$subscriptionCount = $subscriptions.count
+    Write-Host "Found" $subscriptionCount "Subscriptions"
+    $i = 0
+    foreach ($subscription in $subscriptions)
+    {
+      $subValue = $i
+      Write-Host $subValue ":" $subscription.Name "("$subscription.SubscriptionId")"
+      $i++
+    }
+    Do 
+    {
+      [int]$subscriptionChoice = read-host -prompt "Select number & press enter"
+    } 
+    until ($subscriptionChoice -le $subscriptionCount)
+    
+    Write-Host "You selected" $subscriptions[$subscriptionChoice].Name
+    Select-AzSubscription -SubscriptionId $subscriptions[$subscriptionChoice].SubscriptionId
+}
+
+Connect-to-ARM
+
 $cred = Get-Credential
 New-AzResourceGroup `
    -ResourceGroupName "HPrg" `
